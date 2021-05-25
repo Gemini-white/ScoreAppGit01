0d8bc68bdde342d99ac08b12882d18e374fbe195
cheng <2493793849@qq.com>
2021-05-25 20:52:41 +0800

    实现了撤回上一步和重置功能

--- app/src/main/java/com/example/scoreappgit/MainActivity.java
+++ app/src/main/java/com/example/scoreappgit/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.scoreappgit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.example.scoreappgit.databinding.ActivityMainBinding;
@@ -17,12 +18,17 @@ public class MainActivity extends AppCompatActivity {
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
 
+
+        init();
+
         mBinding.buttonAAdd1.setOnClickListener(v -> addA(1));
         mBinding.buttonAAdd2.setOnClickListener(v -> addA(2));
         mBinding.buttonAAdd3.setOnClickListener(v -> addA(3));
         mBinding.buttonBAdd1.setOnClickListener(v -> addB(1));
         mBinding.buttonBAdd2.setOnClickListener(v -> addB(2));
         mBinding.buttonBAdd3.setOnClickListener(v -> addB(3));
+        mBinding.imaButtonReset.setOnClickListener(v -> Reset());
+        mBinding.imaButtonReturn.setOnClickListener(v -> revoke());
 
     }
 
@@ -46,6 +52,30 @@ public class MainActivity extends AppCompatActivity {
         scoreB += i;
         show();
     }
+
+    public void Reset(){
+        save();
+        scoreA = 0;
+        scoreB = 0;
+        show();
+    }
+
+
+    public void init(){
+        scoreA=0;
+        scoreB=0;
+        saveA=0;
+        saveB=0;
+        show();
+    }
+
+    private void revoke(){
+        scoreA=saveA;
+        scoreB=saveB;
+        show();
+    }
+
+
 
 
 }
