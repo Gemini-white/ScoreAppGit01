f068996c38452333ee337033e92aa978b73fd547
cheng <2493793849@qq.com>
2021-05-25 20:19:51 +0800

    实现了AB队+1+2+3功能

--- app/src/main/java/com/example/scoreappgit/MainActivity.java
+++ app/src/main/java/com/example/scoreappgit/MainActivity.java
@@ -4,11 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.scoreappgit.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
+    private int scoreA, scoreB, saveA, saveB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.buttonAAdd1.setOnClickListener(v -> addA(1));
+        mBinding.buttonAAdd2.setOnClickListener(v -> addA(2));
+        mBinding.buttonAAdd3.setOnClickListener(v -> addA(3));
+        mBinding.buttonBAdd1.setOnClickListener(v -> addB(1));
+        mBinding.buttonBAdd2.setOnClickListener(v -> addB(2));
+        mBinding.buttonBAdd3.setOnClickListener(v -> addB(3));
+
     }
+
+    public void save() {
+        saveA = Integer.parseInt(mBinding.scoreA.getText().toString());
+        saveB = Integer.parseInt(mBinding.scoreB.getText().toString());
+    }
+
+    public void show(){
+        mBinding.scoreA.setText(String.valueOf(scoreA));
+        mBinding.scoreB.setText(String.valueOf(scoreB));
+    }
+
+    public void addA(int i) {
+        save();
+        scoreA += i;
+        show();
+    }
+    public void addB(int i){
+        save();
+        scoreB += i;
+        show();
+    }
+
 }
